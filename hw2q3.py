374a40f86a36d81ea9cea032629cd4d8d823b604
Dvir <dvir019@gmail.com>
2019-12-07 17:27:56 +0200

    Add method to get the encoded letter

--- hw2q3.py
+++ hw2q3.py
@@ -1,6 +1,7 @@
 def main():
     sentence = get_sentence()
     shift = get_shift()
+    encoded_sentence = get_encoded_sentence(sentence, shift)
 
 
 def get_sentence():
@@ -15,6 +16,10 @@ def get_shift():
     return shift
 
 
+def get_encoded_sentence(sentence, shift):
+    pass
+
+
 def get_new_char(char, shift):
     new_char = char
     if char == '#':
@@ -23,10 +28,20 @@ def get_new_char(char, shift):
         new_char = ''
     elif char.isalpha():
         new_char = get_new_letter(char, shift)
+    return new_char
 
 
 def get_new_letter(letter, shift):
     letters = [chr(ascii_value) for ascii_value in range(ord('a'), ord('z') + 1)]
+    letter_difference = ord(letter) - ord('a')
+    shift_sign = 1  # Assume that the letter is in even place
+    if (letter_difference + 1) % 2 == 1:  # Check if the letter is in odd place
+        shift_sign = -1
+    new_letter = letters[letter_difference + (shift_sign * (shift % 26))]
+    if shift_sign == -1:
+        new_letter = new_letter.upper()
+
+    return new_letter
 
 
 if __name__ == '__main__':
