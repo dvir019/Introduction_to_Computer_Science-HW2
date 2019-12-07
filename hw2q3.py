28fe9abaa736960fcc95e8869adf56a6f2042f74
Dvir <dvir019@gmail.com>
2019-12-07 22:08:30 +0200

    Start documenting the file

--- hw2q3.py
+++ hw2q3.py
@@ -2,22 +2,54 @@ def main():
     sentence = get_sentence()
     shift = get_shift()
     encoded_sentence = get_encoded_sentence(sentence, shift)
+    print(encoded_sentence)
 
 
 def get_sentence():
+    """
+    Gets a sentence from the user, converts all of the letters to lowercase form,
+    and returns the sentence.
+
+    :return: The lowercase sentence
+    :rtype: str
+    """
     print("Please enter a sentence to encrypt:")
     sentence = input()
-    return sentence.lower()
+    sentence = sentence.lower()
+    return sentence
 
 
 def get_shift():
+    """
+    Gets the shift number from the user, and returns it.
+
+    :return: The shift number
+    :rtype: int
+    """
     print("Please enter the size of the shift:")
     shift = int(input())
     return shift
 
 
 def get_encoded_sentence(sentence, shift):
-    pass
+    """
+    Generates the encoded sentence, and returns it.
+
+    :param sentence: The sentence to encode
+    :type sentence: str
+    :param shift: The shift number
+    :type shift: int
+
+    :return:
+    :rtype:
+    """
+    sentence = sentence.rstrip('!')
+    encoded_sentence_list = []
+    for char in sentence:
+        new_char = get_new_char(char, shift)
+        encoded_sentence_list.append(new_char)
+    encoded_sentence = ''.join(encoded_sentence_list)
+    return encoded_sentence
 
 
 def get_new_char(char, shift):
@@ -37,7 +69,10 @@ def get_new_letter(letter, shift):
     shift_sign = 1  # Assume that the letter is in even place
     if (letter_difference + 1) % 2 == 1:  # Check if the letter is in odd place
         shift_sign = -1
-    new_letter = letters[letter_difference + (shift_sign * (shift % 26))]
+    new_letter_index = letter_difference + (shift_sign * (shift % 26))
+    if new_letter_index >= 26:
+        new_letter_index %= 26
+    new_letter = letters[new_letter_index]
     if shift_sign == -1:
         new_letter = new_letter.upper()
 
