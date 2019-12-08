68f4e8bfc802feed5939ad954803697cbb1dcf27
Dvir <dvir019@gmail.com>
2019-12-08 17:28:37 +0200

    Keep documenting the file, and split the check_row method into two lines.

--- hw2q1.py
+++ hw2q1.py
@@ -6,12 +6,27 @@ def main():
 
 
 def get_board_size():
+    """
+    Gets the size of the board from the user, and returns it.
+
+    :return: The size of the board
+    :rtype: int
+    """
     print("Please enter the side of the board:")
     board_size = int(input())
     return board_size
 
 
 def get_board(board_size):
+    """
+    Gets the sudoku board from the user, and returns it as a 2D list.
+
+    :param board_size: The size of the board
+    :type board_size: int
+
+    :return: The board, as a 2D list
+    :rtype: list[list[int]]
+    """
     board = []
     print("Please enter a solution:")
     for row in range(board_size):
@@ -23,6 +38,15 @@ def get_board(board_size):
 
 
 def is_board_valid(board):
+    """
+    Checks if the whole sudoku board is valid - represents a real solution.
+
+    :param board: The sudoku board, as a 2D list
+    :type board: list[list[int]]
+
+    :return: Whether or not the board is valid
+    :rtype: bool
+    """
     rows_valid = is_rows_valid(board)
     columns_valid = is_columns_valid(board)
     squares_valid = is_squares_valid(board)
@@ -31,6 +55,15 @@ def is_board_valid(board):
 
 
 def is_rows_valid(board):
+    """
+    Checks if all of the rows of the board are valid.
+
+    :param board: The sudoku board, as a 2D list
+    :type board: list[list[int]]
+
+    :return: Whether or not the rows are valid
+    :rtype: bool
+    """
     for row_index in range(len(board)):
         if not check_row(board, row_index):
             return False
@@ -38,6 +71,15 @@ def is_rows_valid(board):
 
 
 def is_columns_valid(board):
+    """
+    Checks if all of the columns of the board are valid.
+
+    :param board: The sudoku board, as a 2D list
+    :type board: list[list[int]]
+
+    :return: Whether or not the columns are valid
+    :rtype: bool
+    """
     for column_index in range(len(board)):
         if not check_column(board, column_index):
             return False
@@ -45,6 +87,15 @@ def is_columns_valid(board):
 
 
 def is_squares_valid(board):
+    """
+    Checks if all of the inner squares of the board are valid.
+
+    :param board: The sudoku board, as a 2D list
+    :type board: list[list[int]]
+
+    :return: Whether or not the inner squares are valid
+    :rtype: bool
+    """
     board_size = len(board)
     square_size = int(board_size ** 0.5)
     for top_index in range(0, board_size, square_size):
@@ -55,10 +106,35 @@ def is_squares_valid(board):
 
 
 def check_row(board, row_to_check):
-    return check_list(board[row_to_check])
+    """
+    Checks if a certain row in the board is valid.
+
+    :param board: The sudoku board, as a 2D list
+    :type board: list[list[int]]
+    :param row_to_check: The row to check
+    :type row_to_check: int
+
+    :return: Whether or not the row is valid
+    :rtype: bool
+    """
+    lst = board[row_to_check]
+    return check_list(lst)
 
 
 def check_column(board, column_to_check):
+    """
+    Checks if a certain column in the board is valid.
+    The method creates a list that contains the numbers of that column,
+    and than check that list.
+
+    :param board: The sudoku board, as a 2D list
+    :type board: list[list[int]]
+    :param column_to_check: The row to check
+    :type column_to_check: int
+
+    :return: Whether or not the column is valid
+    :rtype: bool
+    """
     lst = []
     for row in range(len(board)):
         number = board[row][column_to_check]
